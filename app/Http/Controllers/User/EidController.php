cd1e9a41dfd486fc70440fb103e2c32d7f5fac75
Elias Renman <elias.renman00@gmail.com>
2020-08-08 10:43:29 +0200

    fixed eid controller to comply with user resource

--- app/Http/Controllers/User/EidController.php
+++ app/Http/Controllers/User/EidController.php
@@ -6,15 +6,19 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Vinkla\Hashids\Facades\Hashids;
 use Illuminate\Support\Carbon;
+use \App\User;
+use \App\Http\Resources\UserResource;
+
 
 class EidController extends Controller
 {
     public function update(Request $request)
     {
-        $id = $request->user->id;
-        $user = \App\User::find($id);
-        $user->eid = Hashids::encode(floor(Carbon::now()->timestamp / 2) . $id . rand(0, 10));
+        $githubUser = $request->user;
+        $user = User::findByGithubUser($githubUser);
+        $user->eid = Hashids::encode(floor(Carbon::now()->timestamp / 2) . $githubUser->id . rand(0, 10));
         $user->save();
-        return $user;
+        $subscriptions = $user->pushSubscriptions;
+        return new UserResource(collect($user)->merge($githubUser)->merge(['subscriptions' => $subscriptions]));
     }
 }
